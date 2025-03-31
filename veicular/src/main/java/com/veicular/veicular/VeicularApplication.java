5164c89da51c371c5c8930fe3c19d1efe5e901f0
r_castilho <rafaelcastilhodacosta@gmail.com>
2025-03-31 10:37:26 -0300

    feat(esqueleto-projeto): atvd #01

---
+++ veicular/src/main/java/com/veicular/veicular/VeicularApplication.java
@@ -0,0 +1,13 @@
+package com.veicular.veicular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeicularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeicularApplication.class, args);
+	}
+
+}
---
+++ veicular/src/test/java/com/veicular/veicular/VeicularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veicular.veicular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeicularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
